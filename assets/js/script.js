c326d30f0363078c0113865e417e01f5e49c1640
Emad Saeed <eaf.saeed@gmail.com>
2022-11-20 01:14:50 +0000

    generatePassword

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,67 +6,70 @@ var passwordOptions = {
   upperCase: false,
 }
 
-// Function to prompt user for password options
+var selectedChractersArr = [];
+
+/* FUNCTION TO PROMPT USER FOR PASSWORD OPTIONS */
 function getPasswordOptions() {
 
   /* GET PASSWORD LENGTH */
-  function getPasswordLength() {
-    var passwordLength = prompt('How many characters would you like your password to contain?\nPlease choose a number of at least 10 characters but no more than 64.')
-    console.log(passwordLength);
+  var passwordLength = prompt('How many characters would you like your password to contain?\nPlease choose a number of at least 10 characters but no more than 64.')
 
-    // validate passwordLength
-    if (passwordLength !== null) {
-      if (passwordLength < 10 || passwordLength > 64 || isNaN(passwordLength)) {
-        alert('Please choose a number of at least 10 characters but no more than 64.');
-        getPasswordLength();
-      } else {
-        passwordOptions.passLength = passwordLength;
-      }
+  if (passwordLength !== null) { // run if user not cancel
+    // validate passwordLength input
+    if (passwordLength < 10 || passwordLength > 64 || isNaN(passwordLength)) {
+      alert('Please choose a number of at least 10 characters but no more than 64.');
+      // repeat getPasswordOptions if invalid input
+      getPasswordOptions();
+    } else {
+      // asign value to passwordOptions object
+      passwordOptions.passLength = passwordLength;
     }
+
+    /* GET CHARACTERS TYPE */
+    function getCharactersTypes() {
+
+      /* INCLUDE NUMERIC */
+      var includeNumeric = confirm('Click OK, if you want the password to include Numbers.');
+      // asign value to passwordOptions object
+      if (includeNumeric) passwordOptions.numeric = true;
+
+      /* INCLUDE SPECIAL CHARACTERS */
+      var includeSpecialCharacters = confirm('Click OK, if you want the password to include Special Characters.');
+      // asign value to passwordOptions object
+      if (includeSpecialCharacters) passwordOptions.specialChars = true;
+
+      /* INCLUDE LOWERCASE */
+      var includeLowercase = confirm('Click OK, if you want the password to include Lowercase Characters.');
+      // asign value to passwordOptions object
+      if (includeLowercase) passwordOptions.lowerCase = true;
+
+      /* INCLUDE UPPERCASE */
+      var includeUppercase = confirm('Click OK, if you want the password to include Uppercase Characters.');
+      // asign value to passwordOptions object
+      if (includeUppercase) passwordOptions.upperCase = true;
+    }
+    getCharactersTypes();
+
+    /* VALIDATE CHARACTERS TYPE CHOICES */
+    if (!passwordOptions.numeric && !passwordOptions.specialChars && !passwordOptions.lowerCase && !passwordOptions.upperCase) {
+      alert("Oops! You need to choose at least one type for the password characters!")
+      // Repeat getCharactersTypes if user did not any choice
+      getCharactersTypes();
+    }
+
+    console.log('Password Options Object:', passwordOptions);
+    return passwordOptions;
   }
-  getPasswordLength();
-
-  /* GET CHARACTERS TYPE */
-  function getCharactersType() {
-
-    /* INCLUDE NUMERIC */
-    var includeNumeric = confirm('Click OK, if you want the password to include Numbers.');
-    if (includeNumeric) passwordOptions.numeric = true;
-
-    /* INCLUDE SPECIAL CHARACTERS */
-    var includeSpecialCharacters = confirm('Click OK, if you want the password to include Special Characters.');
-    if (includeSpecialCharacters) passwordOptions.specialChars = true;
-
-    /* INCLUDE LOWERCASE */
-    var includeLowercase = confirm('Click OK, if you want the password to include Lowercase Characters.');
-    if (includeLowercase) passwordOptions.lowerCase = true;
-
-    /* INCLUDE UPPERCASE */
-    var includeUppercase = confirm('Click OK, if you want the password to include Uppercase Characters.');
-    if (includeUppercase) passwordOptions.upperCase = true;
-  }
-  getCharactersType();
-
-  /* VALIDATE CHARACTERS TYPE CHOICES */
-  if (!passwordOptions.numeric && !passwordOptions.specialChars && !passwordOptions.lowerCase && !passwordOptions.upperCase) {
-    alert("Oops! You need to choose at least one type for the password characters!")
-    getCharactersType();
-  }
-
-  console.log(passwordOptions);
-  return passwordOptions;
 }
 
-var choicesArr = [];
+/* FILL SELECTED CHRACTERS ARRAY WITH USER CHOICES */
+function fillSelectedChractersArr() {
+  if (passwordOptions.numeric) selectedChractersArr = numericCharacters.concat(selectedChractersArr);
+  if (passwordOptions.specialChars) selectedChractersArr = specialCharacters.concat(selectedChractersArr);
+  if (passwordOptions.lowerCase) selectedChractersArr = lowerCasedCharacters.concat(selectedChractersArr);
+  if (passwordOptions.upperCase) selectedChractersArr = upperCasedCharacters.concat(selectedChractersArr);
 
-/* CREATEING ARRAY FOR USER CHARACTERS CHOICES */
-function createChoicesArr() {
-  if (passwordOptions.numeric) choicesArr = numericCharacters.concat(choicesArr);
-  if (passwordOptions.specialChars) choicesArr = specialCharacters.concat(choicesArr);
-  if (passwordOptions.lowerCase) choicesArr = lowerCasedCharacters.concat(choicesArr);
-  if (passwordOptions.upperCase) choicesArr = upperCasedCharacters.concat(choicesArr);
-
-  return choicesArr;
+  return selectedChractersArr;
 }
 
 /* FUNCTION FOR GETTING A RANDOM ELEMENT FROM AN ARRAY */
@@ -74,18 +77,28 @@ function getRandom(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
-// Function to generate password with user input
+/* FUNCTION TO GENERATE PASSWORD WITH USER INPUT */
 function generatePassword() {
   getPasswordOptions();
 
-  console.log(createChoicesArr());
+  var selectedChractersArrData = fillSelectedChractersArr();
+  console.log('Selected Chracters Array Data:', selectedChractersArrData);
 
-  // Reset data for another go
-  choicesArr.length = [];
+  var generatedPassword = '';
+
+  for (var i = 0; i < passwordOptions.passLength; i++) {
+    generatedPassword += getRandom(selectedChractersArrData);
+  }
+
+  /* RESET DATA FOR ANOTHER GO */
+  selectedChractersArr.length = [];
+  passwordOptions.passLength = 0;
   passwordOptions.numeric = false;
   passwordOptions.specialChars = false;
   passwordOptions.lowerCase = false;
   passwordOptions.upperCase = false;
+
+  return generatedPassword;
 }
 
 // Get references to the #generate element
