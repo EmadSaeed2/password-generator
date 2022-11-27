686afb745537147e5bf5eda721156b1c4b360fc9
Emad Saeed <eaf.saeed@gmail.com>
2022-11-27 20:16:32 +0000

    fixing bug and some optimization

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,68 +1,72 @@
 var passwordOptions = {
-  passLength: 0,
+  passLength: null,
   numeric: false,
   specialChars: false,
   lowerCase: false,
   upperCase: false,
 }
 
-var selectedChractersArr = [];
-
-/* FUNCTION TO PROMPT USER FOR PASSWORD OPTIONS */
-function getPasswordOptions() {
-
-  /* GET PASSWORD LENGTH */
+/* GET PASSWORD LENGTH */
+function getPasswordLength() {
   var passwordLength = prompt('How many characters would you like your password to contain?\nPlease choose a number of at least 10 characters but no more than 64.')
-
+  // validate password length input
   if (passwordLength !== null) { // run if user not cancel
-    // validate passwordLength input
     if (passwordLength < 10 || passwordLength > 64 || isNaN(passwordLength)) {
       alert('Please choose a number of at least 10 characters but no more than 64.');
-      // repeat getPasswordOptions if invalid input
-      getPasswordOptions();
+      // repeat getPasswordLength if invalid input
+      getPasswordLength();
     } else {
       // asign value to passwordOptions object
       passwordOptions.passLength = passwordLength;
     }
-
-    /* GET CHARACTERS TYPE */
-    function getCharactersTypes() {
-
-      /* INCLUDE NUMERIC */
-      var includeNumeric = confirm('Click OK, if you want the password to include Numbers.');
-      // asign value to passwordOptions object
-      if (includeNumeric) passwordOptions.numeric = true;
-
-      /* INCLUDE SPECIAL CHARACTERS */
-      var includeSpecialCharacters = confirm('Click OK, if you want the password to include Special Characters.');
-      // asign value to passwordOptions object
-      if (includeSpecialCharacters) passwordOptions.specialChars = true;
-
-      /* INCLUDE LOWERCASE */
-      var includeLowercase = confirm('Click OK, if you want the password to include Lowercase Characters.');
-      // asign value to passwordOptions object
-      if (includeLowercase) passwordOptions.lowerCase = true;
-
-      /* INCLUDE UPPERCASE */
-      var includeUppercase = confirm('Click OK, if you want the password to include Uppercase Characters.');
-      // asign value to passwordOptions object
-      if (includeUppercase) passwordOptions.upperCase = true;
-    }
-    getCharactersTypes();
-
-    /* VALIDATE CHARACTERS TYPE CHOICES */
-    if (!passwordOptions.numeric && !passwordOptions.specialChars && !passwordOptions.lowerCase && !passwordOptions.upperCase) {
-      alert("Oops! You need to choose at least one type for the password characters!")
-      // Repeat getCharactersTypes if user did not select any characters option
-      getCharactersTypes();
-    }
-
-    console.log('Password Options Object:', passwordOptions);
-    return passwordOptions;
   }
 }
 
+/* GET CHARACTERS TYPE */
+function getCharactersTypes() {
+
+  /* INCLUDE NUMERIC */
+  var includeNumeric = confirm('Click OK, if you want the password to include Numbers.');
+  // asign value to passwordOptions object
+  if (includeNumeric) passwordOptions.numeric = true;
+
+  /* INCLUDE SPECIAL CHARACTERS */
+  var includeSpecialCharacters = confirm('Click OK, if you want the password to include Special Characters.');
+  // asign value to passwordOptions object
+  if (includeSpecialCharacters) passwordOptions.specialChars = true;
+
+  /* INCLUDE LOWERCASE */
+  var includeLowercase = confirm('Click OK, if you want the password to include Lowercase Characters.');
+  // asign value to passwordOptions object
+  if (includeLowercase) passwordOptions.lowerCase = true;
+
+  /* INCLUDE UPPERCASE */
+  var includeUppercase = confirm('Click OK, if you want the password to include Uppercase Characters.');
+  // asign value to passwordOptions object
+  if (includeUppercase) passwordOptions.upperCase = true;
+
+  /* VALIDATE CHARACTERS TYPE INPUTS */
+  if (!passwordOptions.numeric && !passwordOptions.specialChars && !passwordOptions.lowerCase && !passwordOptions.upperCase) {
+    alert("Oops! You need to choose at least one type for the password characters!")
+    // Repeat getCharactersTypes if user did not select any characters option
+    getCharactersTypes();
+  }
+}
+
+
+/* FUNCTION TO PROMPT USER FOR PASSWORD OPTIONS */
+function getPasswordOptions() {
+  getPasswordLength();
+  if (passwordOptions.passLength !== null) {
+    getCharactersTypes();
+  }
+  console.log('Password Options Object:', passwordOptions);
+  return passwordOptions;
+}
+
 /* FILL SELECTED CHRACTERS ARRAY WITH USER CHOICES */
+var selectedChractersArr = [];
+
 function fillSelectedChractersArr() {
   if (passwordOptions.numeric) selectedChractersArr = numericCharacters.concat(selectedChractersArr);
   if (passwordOptions.specialChars) selectedChractersArr = specialCharacters.concat(selectedChractersArr);
@@ -79,7 +83,6 @@ function getRandom(arr) {
 
 /* FUNCTION TO GENERATE PASSWORD WITH USER INPUT */
 function generatePassword() {
-
   getPasswordOptions();
 
   var selectedChractersArrData = fillSelectedChractersArr();
@@ -93,7 +96,7 @@ function generatePassword() {
 
   /* RESET DATA FOR ANOTHER GO */
   selectedChractersArr = [];
-  passwordOptions.passLength = 0;
+  passwordOptions.passLength = null;
   passwordOptions.numeric = false;
   passwordOptions.specialChars = false;
   passwordOptions.lowerCase = false;
